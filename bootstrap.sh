ef55e8bc37bc06fe1d20c2beaaf79603b20e2005
hqingyi <huangb.qingy@gmail.com>
2015-11-27 20:01:39 +0800

    change: add the git configuration.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 DOTFILE_HOME=$(cd `dirname $0` && pwd)
 
+function link_if_target_not_exists(){
+    if [[ $# -eq 2 ]]; then
+        [[ ! -e $2 ]] && ln -sf $1 $2
+    else
+        echo "invalid args."
+        exit 1
+    fi
+}
+
 function config_spacemacs(){
     cat <<EOF
 === config spacemacs ===
 ..ln ~/.spacemacs
 EOF
-    [[ ! -a ~/.spacemacs ]] && ln -sf $DOTFILE_HOME/spacemacs/.spacemacs ~/.spacemacs
+    link_if_target_not_exists $DOTFILE_HOME/spacemacs/.spacemacs ~/.spacemacs
 }
 
 function config_oh_my_zsh(){
@@ -15,13 +24,24 @@ function config_oh_my_zsh(){
 ..ln ~/.zshrc
 ..install .oh-my-zsh
 EOF
-    [[ ! -d ~/.oh-my-zsh ]] && ln -sf $DOTFILE_HOME/oh-my-zsh/oh-my-zsh ~/.oh-my-zsh
-    [[ ! -a ~/.zshrc ]] && ln -sf $DOTFILE_HOME/oh-my-zsh/.zshrc ~/.zshrc
+    link_if_target_not_exists $DOTFILE_HOME/oh-my-zsh/oh-my-zsh ~/.oh-my-zsh
+    link_if_target_not_exists $DOTFILE_HOME/oh-my-zsh/.zshrc ~/.zshrc
+}
+
+function config_git(){
+    cat <<EOF
+=== config git ===
+.. ln ~/.gitconfig
+.. ln ~/.gitignore_global
+EOF
+    link_if_target_not_exists $DOTFILE_HOME/git/.gitconfig ~/.gitconfig
+    link_if_target_not_exists $DOTFILE_HOME/git/.gitignore_global ~/.gitignore_global
 }
 
 function config_all(){
   config_spacemacs
   config_oh_my_zsh
+  config_git
 }
 
 config_all
